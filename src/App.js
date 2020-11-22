e69bbb999b81b478671dbbcd141cafe4f403c1c5
SriHarshaNagulakonda <sriharshanagulakonda@gmail.com>
2020-11-22 11:11:29 +0530

    image upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {db,auth} from './firebase';
 import { Button, Input } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles'
 import Modal from '@material-ui/core/Modal';
+import ImageUpload from './ImageUpload';
 
 function getModalStyle() {
   const top = 50;
@@ -42,7 +43,7 @@ function App() {
   const [user, setUser]=useState();
 
   useEffect(() => {
-  db.collection('posts').onSnapshot(snapshot => {
+  db.collection('posts').orderBy('timestamp','desc').onSnapshot(snapshot => {
     setPosts(snapshot.docs.map(doc =>({
       id: doc.id,
       post: doc.data()
@@ -167,16 +168,18 @@ function App() {
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
           alt="instagram"
        />
+
+        {user ?(
+          <Button onClick={() => auth.signOut()}>Logout</Button>      
+        ):(
+          <div className="app_loginContainer">
+            <Button onClick={() => {setOpenSign(true)}}>Sign In</Button>
+            <Button onClick={() => {setOpen(true)}}>Sign Up</Button>
+          </div>
+        )}
+
       </div>
 
-      {user ?(
-        <Button onClick={() => auth.signOut()}>Logout</Button>      
-      ):(
-        <div className="app_loginContainer">
-          <Button onClick={() => {setOpenSign(true)}}>Sign In</Button>
-          <Button onClick={() => {setOpen(true)}}>Sign Up</Button>
-        </div>
-      )}
 
       <h1>Helloooooo, Im creating instagram clone with react</h1>
       {
@@ -184,6 +187,12 @@ function App() {
        return   <Post key={id} username={post.username} imageUrl={post.imageUrl} caption={post.caption} />
         })
       }
+
+      {user?.displayName ? (
+        <ImageUpload username={user.displayName}/>
+      ):(
+        <h3>Login to upload</h3>
+      )}
 
     </div>
   );
---
+++ src/ImageUpload.js
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import { Button } from '@material-ui/core'
+import {db,auth, storage} from './firebase';
+import firebase from "firebase";
+import "./ImageUpload.css";
+
+
+function ImageUpload({username}) {
+    const [caption, setCaption] = useState('');
+    const [progress, setProgress] = useState(0);
+    const [image, setImage] = useState();
+
+    const handleChange = (e) => {
+        if(e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    }
+
+    const handleUpload = () => {
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            // progress function ...
+            const progress = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+            setProgress(progress);
+          },
+          (error) => {
+            // Error function ...
+            console.log(error);
+          },
+          () => {
+            // complete function ...
+            storage
+              .ref("images")
+              .child(image.name)
+              .getDownloadURL()
+              .then((url) => {
+                // setUrl(url);
+    
+                // post image inside db
+                db.collection("posts").add({
+                  imageUrl: url,
+                  caption: caption,
+                  username: username,
+                  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                });
+    
+                setProgress(0);
+                setCaption("");
+                setImage(null);
+              });
+          }
+        );
+      };
+    
+    return (
+        <div className="imageUpload">
+            <progress className="imageUpload__progress" value={progress} max="100" />
+            <input type="text" placeholder="Enter a Caption" value={caption} onChange={(e)=> setCaption(e.target.value)} />
+            <input type="file" onChange={handleChange} />
+            <Button onClick={handleUpload}>
+                Upload
+            </Button>        
+        </div>
+    )
+}
+
+export default ImageUpload
